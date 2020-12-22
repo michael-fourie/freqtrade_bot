a6ce63f258cad706b1241794d6baeb8f9eccb07e
Michael Fourie <59344613+michael-fourie@users.noreply.github.com>
2020-12-21 22:52:09 -0800

    Bollinger Bands and RSI Script & Config
    version one.

---
+++ BbandRsi.py
@@ -0,0 +1,69 @@
+# --- Do not remove these libs ---
+from freqtrade.strategy.interface import IStrategy
+from pandas import DataFrame
+import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+from typing import Dict, List
+
+
+# --------------------------------
+
+
+class BbandRsi(IStrategy):
+    """
+
+    author@: Gert Wohlgemuth
+
+    converted from:
+
+    https://github.com/sthewissen/Mynt/blob/master/src/Mynt.Core/Strategies/BbandRsi.cs
+
+    """
+
+    # Minimal ROI designed for the strategy.
+    # adjust based on market conditions. We would recommend to keep it low for quick turn arounds
+    # This attribute will be overridden if the config file contains "minimal_roi"
+    minimal_roi = {
+        "120": 0.01,
+	    "60": 0.025,
+	    "30": 0.03,
+	    "0": 0.05
+    }
+
+    # Optimal stoploss designed for the strategy
+    stoploss = -0.25
+
+    # Optimal timeframe for the strategy
+    timeframe = '1h'
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
+
+        # # Bollinger bands
+        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=2)
+        dataframe['bb_lowerband'] = bollinger['lower']
+        dataframe['bb_middleband'] = bollinger['mid']
+        dataframe['bb_upperband'] = bollinger['upper']
+
+        return dataframe
+
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                    (dataframe['rsi'] < 30) &
+                    (dataframe['close'] < dataframe['bb_lowerband'])
+
+            ),
+            'buy'] = 1
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                    (dataframe['rsi'] > 70) &
+		            (dataframe['close'] > dataframe['bb_upperband'])
+
+            ),
+            'sell'] = 1
+        return dataframe
